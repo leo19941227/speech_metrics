4c8ac40d5779f0ce5b461598c885cd7730991e71
Shu-wen Yang <leo19941227@gmail.com>
2024-07-27 10:59:21 +0800

    add info in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+here = Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
 setup(
     name='speech_metrics',
     version='0.2.0',
     url='https://github.com/leo19941227/speech_metrics',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Shu-wen Yang',
     author_email='leo19941227@gmail.com',
     description='A lightweight package for common metrics in speech processing',
