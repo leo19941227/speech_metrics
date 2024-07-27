3c1eb5bd44c2872a83f4c6e15b7bc3e45db6c3e5
Shu-wen Yang <leo19941227@gmail.com>
2024-07-27 10:52:27 +0800

    add metrics used in SUPERB

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='speech_metrics',
-    version='0.1.0',
+    version='0.2.0',
     url='https://github.com/leo19941227/speech_metrics',
     author='Shu-wen Yang',
     author_email='leo19941227@gmail.com',
